1fb4522790a77aac88992bc61a6a0110940b5cc9
Dennis Stander <djma.stander@gmail.com>
2024-09-09 17:06:17 +0100

    Run Prettier write

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
+import { TextEffect } from "./components/text-effect";
 
-const navigation = [
-  { name: "Projects", href: "/projects" },
-  { name: "Contact", href: "/contact" },
-];
+const navigation = [{ name: "Contact", href: "/contact" }];
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-br from-orange-300 via-orange-400 to-orange-500">
       <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+              className="text-lg duration-500 text-zinc-200 hover:underline"
             >
               {item.name}
             </Link>
@@ -29,23 +27,17 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+        ocean orange
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+        <h2 className="text-md text-zinc-200 ">
+          <TextEffect delay={3.1} per="char" preset="fade">
+            High quality technology services at your disposal.
+          </TextEffect>
         </h2>
       </div>
     </div>
   );
-
 }
